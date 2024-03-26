e9a78aa189f1d80de4e62e577007caed5ee105ba
Jérôme Segura <malwareinfosec@gmail.com>
2024-03-25 18:07:49 -0700

    added new syntax

--- fiddleitm.py
+++ fiddleitm.py
@@ -19,6 +19,7 @@ import random
 
 print('fiddleitm v.0.1')
 
+
 class Fiddleitm:
 
     def __init__(self):
@@ -29,9 +30,10 @@ class Fiddleitm:
         self.anti_vm_list = [
             "VMware", "vmtoolsd", "VMwareService", "Vmwaretray", "vm3dservice",
             "VGAuthService", "Vmwareuser", "TPAutoConnSvc", "VirtualBox", "VBoxService"
-            "VBoxTray", "Fiddler", "FSE2"
+                                                                          "VBoxTray", "Fiddler", "FSE2"
         ]
-        self.do_anti_vm = False #
+        self.do_anti_vm = False
+        rule_type = ""
         """ Load regexes """
         """ master regexes """
         print('Loading master regexes...')
@@ -40,41 +42,48 @@ class Fiddleitm:
         self.regexes_url = 'https://raw.githubusercontent.com/malwareinfosec/fiddleitm/main/regexes.txt'
         response = session.get(self.regexes_url)
 
-        if (response.status_code):
+        if response.status_code:
             data = response.text.split('\r\n')
             self.add_regex_list(data)
         print(' -> master regexes loaded successfully')
 
-        """ local regexes """
-        if os.path.isfile('local_regexes.txt'):
-            print('Loading local regexes...')
-            with open('local_regexes.txt', 'r') as file:
+        """ local rules """
+        if os.path.isfile('local_rules.txt'):
+            print('Loading local rules...')
+            with open('local_rules.txt', 'r') as file:
                 data = file.read().splitlines()
                 self.add_regex_list(data)
-                print(' -> local regexes loaded successfully')
+                print(' -> local rules loaded successfully')
 
     def add_regex_list(self, data):
         for line in data:
             line = line.rstrip('\n')
-            ## Add IP regexes
-            if (line.startswith("IP")):
-                self.IP_data.append(line.split('\t')[1] + ('\t') + line.split('\t')[2])
+            # Add IP regexes
+            if line.startswith("IP"):
+                self.IP_data.append(line.split('\t')[1] + '\t' + line.split('\t')[2])
             # Add URI regexes
-            if (line.startswith("URI")):
-                self.URI_data.append(line.split('\t')[1] + ('\t') + line.split('\t')[2])
+            if line.startswith("URI"):
+                self.URI_data.append(line.split('\t')[1] + '\t' + line.split('\t')[2])
             # Add SourceCode regexes
-            if (line.startswith("SourceCode")):
-                self.SourceCode_data.append(line.split('\t')[1] + ('\t') + line.split('\t')[2])
+            if line.startswith("SourceCode"):
+                self.SourceCode_data.append(line.split('\t')[1] + '\t' + line.split('\t')[2])
 
-    ## Get remote server IP address
-    def get_serverIP(self, flow):
+    # Get remote server IP address
+    def get_serverip(self, flow):
         try:
-            server_IP = flow.server_conn.peername[0]
-        except:
-            server_IP = None
-        return server_IP
+            server_ip = flow.server_conn.peername[0]
+        except Exception:
+            server_ip = None
+        return server_ip
 
-    ## anti-vm
+    def check_conditiontype(self, flow, condition):
+        if condition.startswith('$string='):
+            condition_type = "string"
+        elif condition.startswith('$regex='):
+            condition_type = "regex"
+        return condition_type
+
+    # anti-vm
     def anti_vm(self, flow):
         request_response = flow.request.text
         modified_request_response = request_response
@@ -94,22 +103,22 @@ class Fiddleitm:
                 break
         return modified_request_response
 
-    ## Mark flows
-    def mark_flow(self, flow, regex, type):
+    # Mark flows
+    def mark_flow(self, flow, regex, flow_type):
         """ Play sound """
         print('\a')
         """ Print detection name in console """
-        print(regex.split('\t')[0] + " " + type)
+        print(regex.split('\t')[0] + " " + flow_type)
         """ Mark flow in web UI """
         flow.marked = ":red_circle:"
-        flow.comment = regex.split('\t')[0] + " " + type
+        flow.comment = regex.split('\t')[0] + " " + flow_type
         """ Check if anti-vm detection was detected """
         if "Fingerprinting" in flow.comment:
             self.do_anti_vm = True
         else:
             self.do_anti_vm = False
 
-    ## flow request
+    # flow request
     def request(self, flow):
         """ Do anti-vm """
         if self.do_anti_vm:
@@ -122,52 +131,87 @@ class Fiddleitm:
                 """ Call mark_flow function """
                 self.mark_flow(flow, regex, "[URI]")
 
-    ## flow response
+    # flow response
     def response(self, flow):
         """ Check IP address """
-        server_IP = self.get_serverIP(flow)
-        if server_IP is not None:
+        server_ip = self.get_serverip(flow)
+        if server_ip is not None:
             for regex in self.IP_data:
-                if (server_IP):
-                    ip_match = re.search(regex.split('\t')[1], server_IP)
+                if server_ip:
+                    ip_match = re.search(regex.split('\t')[1], server_ip)
                     if ip_match:
                         """ Call mark_flow function """
                         self.mark_flow(flow, regex, "[IP]")
 
         """ Check response content """
         if flow.response and flow.response.content and "Content-Type" in flow.response.headers and \
-           flow.request.pretty_url != self.regexes_url:
+                flow.request.pretty_url != self.regexes_url:
             if "text" in flow.response.headers["Content-Type"] or "javascript" in flow.response.headers["Content-Type"]:
                 response_match = False
-                for regex in self.SourceCode_data:
-                    """ Check regex type """
-                    # regex contains *AND*
-                    if (" *AND* " in regex):
-                        # split regex into subregexes #
-                        subregexesList = regex.split('\t')[1].split(" *AND* ")
-                        for subregex in subregexesList:
-                            if subregex not in flow.response.text:
-                                # Not all search terms were found
-                                response_match = False
-                                break
-                            response_match = True
-                    # regex contains *OR*
-                    elif (" *OR* " in regex):
-                        # split regex into subregexes #
-                        subregexesList = regex.split('\t')[1].split(" *OR* ")
-                        for subregex in subregexesList:
-                            if subregex in flow.response.text:
-                                # At least one term was found
+
+                for rule in self.SourceCode_data:
+                    condition_type = ""
+                    """ Check rule complexity """
+                    # rule contains *AND*
+                    if " *AND* " in rule:
+                        # split rule into multiple conditions
+                        conditions_list = rule.split('\t')[1].split(" *AND* ")
+                        for condition in conditions_list:
+                            # check condition type (string or regex)
+                            condition_type = self.check_conditiontype(flow, condition)
+                            # string condition
+                            if condition_type == "string":
+                                condition_string = condition.replace('$string="', '')[:-1]
+                                if condition_string not in flow.response.text:
+                                    # Not all search terms were found
+                                    response_match = False
+                                    break
                                 response_match = True
-                                break
-                            response_match = False
+                            # regex condition
+                            elif condition_type == "regex":
+                                condition_string = condition.replace('$regex="', '')[:-1]
+                                response_match = re.search(condition_string, flow.response.text)
+                    # rule contains *OR*
+                    elif " *OR* " in rule:
+                        # split rule into multiple conditions
+                        conditions_list = rule.split('\t')[1].split(" *OR* ")
+                        for condition in conditions_list:
+                            # check condition type (string or regex)
+                            condition_type = self.check_conditiontype(flow, condition)
+                            # string condition
+                            if condition_type == "string":
+                                condition_string = condition.replace('$string="', '')[:-1]
+                                if condition_string in flow.response.text:
+                                    # At least one term was found
+                                    response_match = True
+                                    break
+                                response_match = False
+                            # regex condition
+                            elif condition_type == "regex":
+                                condition_string = condition.replace('$regex="', '')[:-1]
+                                response_match = re.search(condition_string, flow.response.text)
+                                if response_match:
+                                    # At least one term was found
+                                    response_match = True
+                                    break
+                                response_match = False
                     else:
-                        # simple regex #
-                        response_match = re.search(regex.split('\t')[1], flow.response.text)
+                        # simple rule #
+                        # check rule type (string or regex)
+                        condition = rule.split('\t')[1]
+                        condition_type = self.check_conditiontype(flow, condition)
+                        if condition_type == "string":
+                            condition_string = condition.replace('$string="', '')[:-1]
+                            if condition_string in flow.response.text:
+                                response_match = True
+                        elif condition_type == "regex":
+                            condition_string = condition.replace('$regex="', '')[:-1]
+                            response_match = re.search(condition_string, flow.response.text)
 
                     # check if we have a match
                     if response_match:
                         """ Call mark_flow function """
-                        self.mark_flow(flow, regex, "[HTML/JS]")
+                        self.mark_flow(flow, rule, "[HTML/JS]")
+
 
 addons = [Fiddleitm()]
